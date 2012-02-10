44fc6085ac920c349c72083f16e726c02a06a72e
Zack Michener <zack@zackmichener.com>
2012-02-10 17:57:39 -0500

    can now change tag values from modal

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-	$('button').click(function() {
-		$('#change-tag').slideDown();
+	var cur_label;
 
-		var cell = $(this).parent();
-		console.log($(this).parent().children('.tag-header'));
+	$('button').click(function() {
+		cur_label = $(this).parent().children('.tag-header');
+		$('#name_header').text(cur_label.text());
+		$('#modal-input').val(cur_label.text());
+	});
+
+	$('.modal-footer .btn.primary').click(function() {
+		var modal_input = $('#modal-input');
+		cur_label.text(modal_input.val());
+		$('#modal-from-dom').modal('hide');
+	});
+
+	$('.modal-footer .btn.cancel').click(function() {
+		$('#modal-from-dom').modal('hide');
 	});
 });
