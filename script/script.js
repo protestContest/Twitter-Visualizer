723d81fa330bc83a001c2ada77d861f73b194da0
Zack Michener <zack@zackmichener.com>
2012-02-10 22:49:32 -0500

    started to implement changing spotters

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	var cur_label;
+	var cur_col;
 	var spotters = [10];
 
 	// sets up default spotters
@@ -8,6 +9,7 @@ $(document).ready(function() {
 	// brings up modal with info of clicked cell
 	$('button').click(function() {
 		cur_label = $(this).parent().children('.tag-header');
+		cur_col = $(this).parent().index();
 		$('#name_header').text(cur_label.text());
 		$('#modal-input').val(cur_label.text());
 	});
@@ -16,6 +18,18 @@ $(document).ready(function() {
 	$('.modal-footer .btn.primary').click(function() {
 		var modal_input = $('#modal-input');
 		cur_label.text(modal_input.val());
+
+		spotters[cur_col].stop();
+		spotters[cur_col] = new Spotter("twitter.search",
+			{q:modal_input.val(), period:100},
+			{buffer:true, bufferTimeout:1000});
+		spotters[cur_col].tweets = [];
+
+		registerTweets(spotters[cur_col], cur_col, modal_input.val());
+		spotters[cur_col].start();
+
+		$('.col' + cur_col).remove();
+
 		$('#modal-from-dom').modal('hide');
 	});
 
@@ -33,10 +47,12 @@ function init_spotters(spotters) {
 		spotters[i] = new Spotter("twitter.search",
 			{q:query, period:100},
 			{buffer:true, bufferTimeout:1000});
+			
+		spotters[i].query = query;
 		spotters[i].tweets = [];
 		spotters[i].maxtweets = Math.floor(0.1 * ($(document).height() - $('#info-row').height()));
 			
-		registerTweets(spotters[i], i, query);
+		registerTweets(spotters[i], i, spotters[i].query);
 		spotters[i].start();
 	}
 }
@@ -47,8 +63,12 @@ function registerTweets(s, i, query) {
 	// var numtweets = 0;
 	
 	s.register(function(tweet) {
+		var query = $('.tag-header')[i].textContent;
+		if (s.query != query)
+			return;
+
 		// makes new  box representing tweet
-		var new_tweetbox = $("<div class=tweetbox>"+ tweet.text +"</div>");
+		var new_tweetbox = $("<div class='tweetbox col" + i + "'>"+ tweet.text +"</div>");
 
 		if (i === 9)
 			new_tweetbox.addClass('last');
